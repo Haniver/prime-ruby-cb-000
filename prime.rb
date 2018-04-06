3840187761a748f3a14a9d08ca4c42fdde7a2bbd
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-06 16:51:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'pry'
+def prime?(number)
+
+end
+
+def generate_primes
+  primes = [2]
+  for i in 1..100000000
+    j
+    primes
+  end
+end
+
+def factorial(number)
+  while number > 1 do
+    binding.pry
+    factorial(number-1) * number
+  end
+end
+
+binding.pry
